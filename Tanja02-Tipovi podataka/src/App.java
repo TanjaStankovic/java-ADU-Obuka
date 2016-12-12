9c81ff6d12b28926d413b453c39d761a00263c32
TanjaStankovic <tanja.pisces@gmail.com>
2016-12-12 15:06:56 +0100

    drugi commit

---
+++ Tanja02-Tipovi podataka/src/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int myNumber =88;
+short myshort=847;
+long mylong=9797;
+double mydouble= 7.656; 
+
+System.out.println(myNumber);
+System.out.println(myshort);
+System.out.println(mylong);
+System.out.println(mydouble);
+	}
+
+}
