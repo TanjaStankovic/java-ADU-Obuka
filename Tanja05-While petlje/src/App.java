4ce77a7652aa2c53cfb063665595596a3c11c7bd
TanjaStankovic <tanja.pisces@gmail.com>
2016-12-13 10:33:17 +0100

    Trostruka petlja

---
+++ Tanja05-While petlje/src/App.java
@@ -0,0 +1,35 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int value = 0;
+int value1= 30;
+
+int value2=50;
+	    
+     while(value < 101)
+       {
+          System.out.println(value);
+            
+           value = value + 1;
+       }
+     
+       
+      while(value1 < 51)
+      {
+    	   System.out.println(value1);
+           
+         value1 = value1 + 1;
+     }
+        
+       
+       while(value2 >29)
+       {
+    	   System.out.println(value2);
+           
+           value2 = value2 - 1;
+       }
+	}
+	}
+
