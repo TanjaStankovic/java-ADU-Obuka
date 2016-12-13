cd4b719985de84f9c3795c71347a643c733922be
TanjaStankovic <tanja.pisces@gmail.com>
2016-12-13 09:42:26 +0100

    samostalan program

---
+++ Tanja03-Program samostalni/src/app.java
@@ -0,0 +1,14 @@
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String[] imena ={"Tanja Stankovic", "Sanja Katan-Grbic", "Mladen Tosic","Dragana Pupac","Milan Latinovic"
+};
+for( String imena1 : imena)
+{
+	System.out.println(imena1 + "");
+}
+	}
+
+}
